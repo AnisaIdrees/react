642dbe3dfbf218250ceda131d290350daa146776
AnisaIdrees <AnisaIdrees191@gmail.com>
2025-03-22 04:07:05 +0500

    class based component and notes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 
-import { Component } from "react";
-class App extends Component {
-  constructor(props){
-    super(props)
+//------- class component -------//
+
+// 1) import krna hoga Component ko jo react se arha he .
+// 2) React me component ek built-in class se ati he or App Component usko extend krrha he ....
+
+/* class App extends Component {
+     constructor(props){
+     super(props)
+     }
+     render(){ 
+     return(
+     <h1>hello</h1>
+     )
   }
-  render(){
-    return(
-    <div>
-      <h1>hello world</h1>
-    </div>
-    )
-  }
-}
-export default App
+}*/
+
+/*3)Jab hum class-based component use karti hain, 
+to render() method React ke liye zaroori hai kyunki yahi method UI return karta hai.
+
+✅ render() ka kaam:
+
+*Component ka UI define karta hai.
+*React ko batata hai ke kya show karna hai.
+* HTML (JSX) return karta hai jo DOM me mount hota hai.*/
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+////----------------------------code ---------------------------------///
+
+// import { Component } from "react";
+// class App extends Component {
+//   constructor(props){
+//     super(props)
+//   }
+//   render(){
+//     return(
+//     <div>
+//       <h1>hello world</h1>
+//     </div>
+//     )
+//   }
+// }
+// export default App
