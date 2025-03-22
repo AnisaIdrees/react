c728730fdbbb74059b2b142a6bca395d3e9e5e1f
AnisaIdrees <AnisaIdrees191@gmail.com>
2025-03-23 02:07:24 +0500

    named export (multiple class component) / default export (one export class component , React.Fragment)

--- src/App.js
+++ src/App.js
@@ -29,17 +29,64 @@ to render() method React ke liye zaroori hai kyunki yahi method UI return karta 
 
 ////----------------------------code ---------------------------------///
 
-// import { Component } from "react";
-// class App extends Component {
-//   constructor(props){
-//     super(props)
-//   }
-//   render(){
-//     return(
-//     <div>
-//       <h1>hello world</h1>
-//     </div>
-//     )
-//   }
-// }
-// export default App
+import { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+      </div>
+    )
+  }
+}
+export default App // one default export class Component
+
+
+
+export class App2 extends Component { // multiple export class based Component 
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h1>hello Aneesa</h1>
+    )
+  }
+}
+
+export class Paragraph extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <p>Hello! Aneesa is a passionate and dedicated developer currently working
+        on a Blogging Platform using HTML, CSS, JavaScript (ES6), and Firebase.
+        She has already set up authentication and is now focusing on storing and
+        displaying blog posts in Firestore, including categories, titles, descriptions,
+        and content. Aneesa has a strong command of HTML, CSS, JavaScript, and Firebase
+        (Firestore + Auth) and is continuously improving her skills. She prefers step-
+        by-step guidance when learning new technologies and aims to master Firestore in
+        an easy and efficient way. With a keen interest in remote jobs and startup development,
+        Aneesa actively tracks in-demand technologies and platforms like HiringMine, Toptal,
+        LinkedIn, Glassdoor, and Upwork. Her ultimate goal is to achieve a five-digit salary
+        and establish herself as a successful developer while balancing her team collaborations
+        and independent projects. 🚀</p>
+    )
+  }
+}
+
+export class InputField extends Component {
+  constructor (props){
+    super(props)
+  }
+  render(){
+    return(
+
+      <input type="date"></input>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import {App} from './App'
-import App from './App';
+import App, { App2, Paragraph, InputField } from './App'
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+
+  // <React.Fragment>
+  <>
     <App />
-  </React.StrictMode>
+    <App2 />
+    <Paragraph />
+    <InputField />
+
+    <div>hello</div>
+    <div>how are you</div>
+    <div>I'm fine</div>
+    <div>WE are also fine</div>
+  </>
+  // </React.Fragment>
+  // </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
